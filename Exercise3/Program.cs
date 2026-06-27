4776ed866f0036a40d180fac6e309d47c835587e
Eduarjn <eduarjose.fajardo96@gmail.com>
2026-06-27 13:10:38 -0300

    Add Exercise3/Program.cs

---
+++ Exercise3/Program.cs
@@ -0,0 +1,22 @@
+Random randomGenerator = new Random();
+int magicNumber = randomGenerator.Next(1, 101);
+int guess = 0;
+
+while (guess != magicNumber)
+{
+    Console.Write("What is your guess? ");
+    guess = int.Parse(Console.ReadLine());
+
+    if (guess < magicNumber)
+    {
+        Console.WriteLine("Higher");
+    }
+    else if (guess > magicNumber)
+    {
+        Console.WriteLine("Lower");
+    }
+    else
+    {
+        Console.WriteLine("You guessed it!");
+    }
+}
