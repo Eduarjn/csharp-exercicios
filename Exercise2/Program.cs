35c763de4278a2fe360044f92159d7b82fb50ca3
Eduarjn <eduarjose.fajardo96@gmail.com>
2026-06-27 13:10:02 -0300

    Add Exercise2/Program.cs

---
+++ Exercise2/Program.cs
@@ -0,0 +1,36 @@
+Console.Write("What is your grade percentage? ");
+int grade = int.Parse(Console.ReadLine());
+
+string letter;
+
+if (grade >= 90)
+{
+    letter = "A";
+}
+else if (grade >= 80)
+{
+    letter = "B";
+}
+else if (grade >= 70)
+{
+    letter = "C";
+}
+else if (grade >= 60)
+{
+    letter = "D";
+}
+else
+{
+    letter = "F";
+}
+
+Console.WriteLine($"Your grade is {letter}");
+
+if (grade >= 70)
+{
+    Console.WriteLine("Congratulations, you passed the course!");
+}
+else
+{
+    Console.WriteLine("Keep working hard, you'll get it next time.");
+}
