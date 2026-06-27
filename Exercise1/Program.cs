9c6cea7fc98c41c287dd37a0c3378a3215bb3f36
Eduarjn <eduarjose.fajardo96@gmail.com>
2026-06-27 13:09:14 -0300

    Add Exercise1/Program.cs

---
+++ Exercise1/Program.cs
@@ -0,0 +1,5 @@
+Console.Write("What is your first name? ");
+string firstName = Console.ReadLine();
+Console.Write("What is your last name? ");
+string lastName = Console.ReadLine();
+Console.WriteLine($"\nYour name is {lastName}, {firstName} {lastName}.");
