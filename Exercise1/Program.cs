81bb0102f9b6a4e5ad66f1ea0935c265410d3be9
Eduarjn <eduarjose.fajardo96@gmail.com>
2026-06-28 20:46:52 -0300

    Update prompt and output to include nickname

--- Exercise1/Program.cs
+++ Exercise1/Program.cs
@@ -1,5 +1,5 @@
 Console.Write("What is your first name? ");
 string firstName = Console.ReadLine();
-Console.Write("What is your last name? ");
+Console.Write("What is your last name and nickname? ");
 string lastName = Console.ReadLine();
-Console.WriteLine($"\nYour name is {lastName}, {firstName} {lastName}.");
+Console.WriteLine($"\nYour name is {lastName}, {firstName} {lastName} {Nickname}.");
